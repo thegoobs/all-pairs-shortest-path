a65cc2d6dbe2ce61a5d8f11c6d75d0caf15b592e
thegoobs <guthrieschoolar95@gmail.com>
2018-03-26 11:00:10 -0500

    Working on reading a cost matrix from a file

--- graph.cpp
+++ graph.cpp
@@ -1,34 +1,76 @@
-typedef vector<vector <int> > cost_matrix;
+typedef vector<vector <int> > cost_matrix; //nifty way to not type vector so many times
 
-cost_matrix generate_graph(int rows, int columns) {
+cost_matrix generate_graph(int rows = 1, int columns = 1) {
 	cost_matrix g(columns);
 
 	for (int i = 0; i < rows; i++) {
 		g[i] = vector<int>(columns);
 		for (int j = 0; j < columns; j++) {
-			g[i][j] = i * j;
+			g[i][j] = 1;
 		}
 	}
 	return g;
 }
 
 void print_graph(cost_matrix graph) {
+	//get dimensions
 	int rows = graph.size();
 	int columns = graph[0].size();
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
-			cout << graph[i][j] << "\t";
+			cout << graph[i][j] << " ";
 		}
 		cout << endl;
 	}
-}
-
-void write_graph() {
 
 	return;
 }
 
-void read_graph() {
-	
+void write_graph_to_file(string file_name, cost_matrix graph) {
+	//open file with ostream and get a backup of the cout buffer
+	ofstream fp(file_name, ios::out);
+	streambuf* backup = cout.rdbuf();
+
+	//if the file is opened
+	if (fp.is_open()) {
+		//change cout to write directly to file
+		cout.rdbuf(fp.rdbuf());
+		//print graph now writes to file_name
+		print_graph(graph);
+	}
+
+	cout.rdbuf(backup); //remember to put cout back!
+	fp.close();
+
 	return;
+}
+
+cost_matrix read_graph_from_file(string file_name) {
+	cost_matrix graph = generate_graph();
+	string s;
+	//open file for reading and get stream buffer
+	ifstream fp(file_name, ios::in);
+	streambuf* backup = cin.rdbuf();
+	int i = 0, j = 0;
+
+	if (fp.is_open()) {
+		cin.rdbuf(fp.rdbuf());
+
+		while (getline(cin, s)) {
+			vector<int> row;
+			stringstream ss(s);
+
+			graph.push_back(vector<int>(1));
+
+			int value;
+			while (ss >> value) {
+				graph[i].push_back(value);
+			}
+			i++;
+		}
+	}
+
+	cin.rdbuf(backup);
+	fp.close();
+	return graph;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
 using namespace std;
 
 #include "graph.cpp"
 
 int main(int argc, char* argv[]) {
 	cost_matrix g = generate_graph(10, 10);
-	print_graph(g);
+	// print_graph(g);
+
+	cost_matrix	g2 = read_graph_from_file("example.txt");
+	print_graph(g2);
 }
